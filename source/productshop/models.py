ee09ae39a0fa42c217a35d37c761ae6511f2d6af
Nooruzbay <nooruzbay@gmail.com>
2022-01-07 16:19:42 +0600

    changed the migrations

--- source/productshop/models.py
+++ source/productshop/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-CHOICES = [('grocery', 'Grocery'), ('cleaning_supplies', 'Cleaning Supplies'), ('stationary', 'Stationary'), ('butchers', 'Butchers')]
+CHOICES = [('other', 'Other'), ('grocery', 'Grocery'), ('cleaning_supplies', 'Cleaning Supplies'), ('stationary', 'Stationary'), ('butchers', 'Butchers')]
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Name")
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Description")
-    category = models.TextField(default="Other", choices=CHOICES, null=False, blank=False, verbose_name="Category")
+    category = models.TextField(default=CHOICES[0], choices=CHOICES, null=False, blank=False, verbose_name="Category")
     remainder = models.PositiveIntegerField(null=False, blank=False, verbose_name="Remainder")
     price = models.DecimalField(null=False, blank=False, max_digits=7, decimal_places=2, verbose_name="Price")
 
